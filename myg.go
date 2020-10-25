c3d230a1ca875ee08936e5e55a7cd914582a6235
Quentin Rasmont <qrasmont@gmail.com>
2020-10-25 12:23:10 +0100

    Define the command line flags and expeted args

--- myg.go
+++ myg.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "fmt"
+    "os"
+)
+
+func commandeUsage() {
+    fmt.Printf("Usage: myg [ OPTIONS ] TOPICS\n\n")
+    fmt.Printf("Where TOPICS are the mqtt topics to subscribe to.\n\n")
+    fmt.Printf("Where OPTIONS are:\n")
+    flag.PrintDefaults()
+}
 
 func main() {
-    fmt.Println("Hi, I'm myg!")
+    // Define the command flags.
+    address := flag.String("a", "tcp://localhost", "mqtt broker address")
+    port := flag.Int("p", 1883, "mqtt broker port")
+
+    // Set a custom usage message.
+    flag.Usage = commandeUsage
+
+    // Prase the flags.
+    flag.Parse()
+
+    // Check we have at least one topic to subscribe to.
+    if len(os.Args) < 2 {
+        fmt.Printf("I need at least one topic :(\n\n")
+        commandeUsage()
+        os.Exit(1)
+    }
+
+    fmt.Printf("address: %s, port: %d\n", *address, *port)
 }
