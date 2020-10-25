46c2aef78c8db334241d1fdecd3714f81b97ecb0
Quentin Rasmont <qrasmont@gmail.com>
2020-10-25 18:03:06 +0100

    Use the Go coding rules
        Golang has a gofmt tools that apply the "official" coding rules, so
        use it

--- myg.go
+++ myg.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func commandeUsage() {
-    fmt.Printf("Usage: myg [ OPTIONS ] TOPICS\n\n")
-    fmt.Printf("Where TOPICS are the mqtt topics to subscribe to.\n\n")
-    fmt.Printf("Where OPTIONS are:\n")
-    flag.PrintDefaults()
+	fmt.Printf("Usage: myg [ OPTIONS ] TOPICS\n\n")
+	fmt.Printf("Where TOPICS are the mqtt topics to subscribe to.\n\n")
+	fmt.Printf("Where OPTIONS are:\n")
+	flag.PrintDefaults()
 }
 
 func main() {
-    // Define the command flags.
-    address := flag.String("a", "localhost", "mqtt broker address")
-    port := flag.Int("p", 1883, "mqtt broker port")
+	// Define the command flags.
+	address := flag.String("a", "localhost", "mqtt broker address")
+	port := flag.Int("p", 1883, "mqtt broker port")
 
-    // Set a custom usage message.
-    flag.Usage = commandeUsage
+	// Set a custom usage message.
+	flag.Usage = commandeUsage
 
-    // Parse the flags.
-    flag.Parse()
+	// Parse the flags.
+	flag.Parse()
 
-    // Check we have at least one topic to subscribe to.
-    if len(os.Args) < 2 {
-        fmt.Printf("I need at least one topic :(\n\n")
-        commandeUsage()
-        os.Exit(1)
-    }
+	// Check we have at least one topic to subscribe to.
+	if len(os.Args) < 2 {
+		fmt.Printf("I need at least one topic :(\n\n")
+		commandeUsage()
+		os.Exit(1)
+	}
 
-    setSubTopics(os.Args[1:]) // Slice to ignore our path
-    setMQTTConParam(*address, *port)
-    startMQTTClient()
+	setSubTopics(os.Args[1:]) // Slice to ignore our path
+	setMQTTConParam(*address, *port)
+	startMQTTClient()
 }
